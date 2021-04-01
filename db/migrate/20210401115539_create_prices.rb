55083152d365ffa9fdf2b204d7c56583cb5c8a7b
terakura aina <aicaierwei@gmail.com>
2021-04-01 23:26:17 +0900

    コントローラ、ビューを作成

--- db/migrate/20210401115539_create_prices.rb
+++ db/migrate/20210401115539_create_prices.rb
@@ -1,9 +1,7 @@
 class CreatePrices < ActiveRecord::Migration[6.1]
   def change
     create_table :prices do |t|
-      t.integer :grade, null: false
-      t.datetime :start_day_at, null: false
-      t.datetime :finish_day_at, null: false
+      t.text :grade, null: false
 
       t.timestamps
     end
